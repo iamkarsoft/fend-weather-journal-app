0dbff9e220fe548c94ed1d2a63a8fc84ea7d50a3
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-08-30 09:52:19 +0000

    wip

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 // Personal API Key for OpenWeatherMap API
+const baseUrl = `http://api.openweathermap.org/data/2.5/weather?zip=`
+const apiKey = '&appid=7cabf75ed9d657f73006ef6a549f1209'
+
+// Create a new date instance dynamically with JS
+const d = new Date();
+const newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
 
 // Event listener to add function to existing HTML DOM element
+const zipInput = document.querySelector("#zip").value
+const feelingsInput = document.querySelector("#feelings")
+const generateButton = document.querySelector("#generate")
+const entryHolder = document.querySelector("#entryHolder")
+const dateContainer = document.querySelector("#date")
+const tempContainer = document.querySelector("#temp")
+const contentContainer = document.querySelector("#content")
+
+function buildDom(e){
+	e.preventDefault();
+	dateContainer.textContent = newDate;
+	contentContainer.textContent = feelingsInput.value;
+
+}
+
+
+const apiData = async (baseUrl,zipInput,apiKey)=>{
+		console.log(`${baseUrl}${zipInput}${apiKey}`)
+		// const response = await fetch(`${baseUrl}${zipInput}${apiKey}`)
+		// const data = await response.json();
+		// console.log(data);
+		
+}
+
+// generateButton.addEventListener('click',buildDom);
+generateButton.addEventListener('click',apiData);
+
+
+/* Function to GET Web API Data*/
+
+
 
 /* Function called by event listener */
 
-/* Function to GET Web API Data*/
+
+
 
 /* Function to POST data */
 
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 // Setup empty JS object to act as endpoint for all routes
+projectData = {};
 // Express to run server and routes
-
+const express = require('express')
 // Start up an instance of app
 
+const app  = express()
+
 /* Dependencies */
+const bodyParser = require('body-parser')
+
 /* Middleware*/
+app.use(bodyParser.urlencoded({extended:false}))
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
+const cors = require('cors')
+
+app.use(cors())
 
 // Initialize the main project folder
+app.use(express.static('.'))
 
 // Spin up the server
+const port = "8967"
+const server = app.listen(port, ()=>{console.log(`running on localhost:${port}`)})
+
 // Callback to debug
 
 // Initialize all route with a callback function
 
 // Callback function to complete GET '/all'
+function dataGet(){
+	
+}
 
 // Post Route
-  
